f777d07b1e02b82b09a080e798cb961eb468f8b9
Verbeerst Thibo <thibo.verbeerst@student.howest.be>
2024-01-14 23:34:20 +0000

    refactor

--- src/main.py
+++ src/main.py
@@ -1,9 +1,3 @@
-# The steps implemented in the object detection sample code: 
-# 1. for an image of width and height being (w, h) pixels, resize image to (w', h'), where w/h = w'/h' and w' x h' = 262144
-# 2. resize network input size to (w', h')
-# 3. pass the image to network and do inference
-# (4. if inference speed is too slow for you, try to make w' x h' smaller, which is defined with DEFAULT_INPUT_SIZE (in object_detection.py or ObjectDetection.cs))
-"""Sample prediction script for TensorFlow 1.x."""
 import sys
 from dotenv import load_dotenv
 import os
